fa99a22b9c2ebb3c78b5e28437fb30e239501e71
anantsharma9430228931 <anantsharma9430228931@gmail.com>
2021-05-06 22:39:58 +0530

    Second init

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+const App = () => {
+  return (
+    <div id="main">I am learning React. My life is getting better.</div>
+  )
+}
+
+
+export default App;
